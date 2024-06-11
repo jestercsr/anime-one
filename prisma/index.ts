987eb926978611c0b8b83358be8e5db71a699c82
jester cesar <jester.csr@gmail.com>
2024-06-11 10:14:01 +0200

    maj prisma 3

--- prisma/index.ts
+++ prisma/index.ts
@@ -2,7 +2,7 @@
 import { PrismaClient } from "@prisma/client";
 
 
-export async function getAllManga(id) {
+export async function getAllManga(id: any) {
   const prisma = new PrismaClient();
   const allManga = await prisma.mangaName.findUnique({
     where: {
