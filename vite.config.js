25ef820c5261018abf2977212b6457a9d7d9929e
jestercsr <jester.csr@gmail.com>
2026-07-20 22:25:53 +0200

    ete 2026 4

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import vike from 'vike/plugin'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
